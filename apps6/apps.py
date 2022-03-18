113ce0995b2eb544d2a514c417cfbfbf99aebf22
vijaypathipati <vijaypathipati19165@gmail.com>
2022-03-18 10:42:18 +0530

    first commit

---
+++ apps6/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Apps6Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps6'
---
+++ apps6/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def aravind(request):
+    return HttpResponse('aravind is a good boy')
